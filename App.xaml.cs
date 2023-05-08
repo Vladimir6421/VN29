be4a12502338f90490d1549da25924223d5dd6f4
Владимир Державин <Derzhavinonline@ya.ru>
2023-05-08 21:38:30 +0300

    Тестовый проект настройка

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows;
 
 namespace VN29
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
     }
