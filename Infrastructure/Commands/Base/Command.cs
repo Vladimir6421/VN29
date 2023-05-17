a23abd85fa25de1f489a42a52d38b77d06bf4071
Владимир Державин <Derzhavinonline@ya.ru>
2023-05-17 23:43:09 +0300

    Базовый класс команды

--- Infrastructure/Commands/Base/Command.cs
+++ Infrastructure/Commands/Base/Command.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace VN29.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
